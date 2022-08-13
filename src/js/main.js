225b0a1f694b78b1ea92ef8dab2bcd6ffe82e6d9
Alexandr874 <aleks.ribinsk@mail.ru>
2022-08-13 20:35:45 +0300

    add timer

--- src/js/main.js
+++ src/js/main.js
@@ -55,6 +55,55 @@ window.addEventListener('DOMContentLoaded', () => {
           }); 
 
 
+    //timer
+
+          const deadline = '2022-08-18';
+
+          function getTimRimaining(endtaim) {
+            const t = Date.parse(endtaim) - Date.parse(new Date()),
+                  days = Math.floor(t / (1000 * 60 * 60 * 24)),
+                  hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+                  minutes = Math.floor((t / 1000 / 60) % 60),
+                  seconds = Math.floor((t / 1000) % 60);
+
+                  return {
+                    'total': t,
+                    'days': days,
+                    'hours': hours,
+                    'minutes': minutes,
+                    'seconds': seconds 
+                  };
+
+          }
+
+
+          function setClock(selector, endtaim) {
+             const timer = document.querySelector(selector),
+                   days = timer.querySelector('#days'),
+                   hours = timer.querySelector('#hours'),
+                   minutes = timer.querySelector('#minutes'),
+                   seconds = timer.querySelector('#seconds'),
+                   timeInterval = setInterval(updateClock, 1000);
+
+
+
+              function updateClock() {
+                const t = getTimRimaining(endtaim);
+                days.innerHTML = t.days;
+                hours.innerHTML = t.hours;
+                minutes.innerHTML = t.minutes;
+                seconds.innerHTML = t.seconds;
+
+                if (t.total <= 0) {
+                  clearInterval(timeInterval);
+                }
+              }
+
+          }
+
+          setClock('.timer', deadline);
+
+
 
           
         
