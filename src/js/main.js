dd35fee3fa98f7c5418cd452e209ad5ecae4b4fc
Alexandr874 <aleks.ribinsk@mail.ru>
2022-08-13 19:27:34 +0300

    add tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 
 'use stric';
 
+
+
 window.addEventListener('DOMContentLoaded', () => {
 
 
@@ -9,9 +11,56 @@ window.addEventListener('DOMContentLoaded', () => {
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
+
+          function hidenTabsContent() {
+            tabsContent.forEach(item => {
+                item.classList.add('hide');
+                item.classList.remove('sshow', 'fade');
+            });
+
+            tabs.forEach(item => {
+                item.classList.remove('tabheader__item_active');
+
+            });
+          }
+          hidenTabsContent();
+
+          function showTabsContent(i = 0) {
+            tabsContent[i].classList.add('show', 'fade');
+            tabsContent[i].classList.remove('hide');
+
+            tabs[i].classList.add('tabheader__item_active');
+          }
+
+
+
+          hidenTabsContent();
+          showTabsContent();
+
+
+          tabsParent.addEventListener('click', (event) => {
+
+            const target = event.target;
+
+            if (target && target.classList.contains('tabheader__item')) {
+
+                    tabs.forEach((item, i) => {
+
+                        if(target === item) {
+                            hidenTabsContent();
+                            showTabsContent(i);
+                        }
+
+                    });
+            }
+
+          }); 
+
+
+
           
         
-   
+
 
 
 });
