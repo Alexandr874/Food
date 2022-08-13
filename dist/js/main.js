c7ba94ed27213463479e2424aacbd3e8c4f13b72
Alexandr874 <aleks.ribinsk@mail.ru>
2022-08-13 12:58:33 +0300

    мой первый коммит

--- dist/js/main.js
+++ dist/js/main.js
@@ -100,6 +100,7 @@ window.addEventListener('DOMContentLoaded', () => {
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
         tabsParent = document.querySelector('.tabheader__items');
+  jkhkjhjkh;
 });
 
 /***/ })
--- src/js/main.js
+++ src/js/main.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', () => {
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
           
-
+jkhkjhjkh
    
 
           
