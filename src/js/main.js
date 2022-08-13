332fcf1e52917f086619fef36e37d6614d18e882
Alexandr874 <aleks.ribinsk@mail.ru>
2022-08-13 12:48:33 +0300

    init

---
+++ src/js/main.js
@@ -0,0 +1,32 @@
+
+'use stric';
+
+window.addEventListener('DOMContentLoaded', () => {
+
+
+    // tabs
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+          
+
+   
+
+          
+
+});
+    
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
